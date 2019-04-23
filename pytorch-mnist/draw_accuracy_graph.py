ee8b2c169ca1331e435ececc507b0597ba08f21e
kishkash555 <kishkash555@gmail.com>
2019-04-23 16:05:21 +0300

    change graphing code

--- pytorch-mnist/draw_accuracy_graph.py
+++ pytorch-mnist/draw_accuracy_graph.py
@@ -1,4 +1,5 @@
 import argparse
+from collections import defaultdict
 import glob
 from os import path
 import matplotlib.pyplot as plt
@@ -12,7 +13,7 @@ def arguments():
                     help='directory where files reside. optional/')
     parser.add_argument('-g','--glob', type=str, default='', metavar='G',
                     help='pattern for files')
-
+    parser.add_argument('-s','--show-by',type=str, default='epoch', help='graph by epoch or layer size')
     arg = parser.parse_args()
     return arg
 
@@ -23,11 +24,12 @@ def parse_result_log(fo):
     print("layer_size: {}".format(layer_size))
     correct=[]
     for line in fo:
-        correct.append(int(line.split(" ")[2]))
+        if line.startswith('Test:'):
+            correct.append(int(line.split(" ")[2]))
     return layer_size, correct
 
 
-def load_results(dir, files):
+def load_results_by_epoch(dir, files):
     res = {}
     for file_name in files:
         full_fname = path.join(dir,file_name.strip())
@@ -36,22 +38,44 @@ def load_results(dir, files):
             res[layer_size] = [1-float(c)/10000 for c in correct]
     return res
 
-def plot_results(res):
+def load_results_by_layer_size(dir, files):
+    res = defaultdict(list)
+    for file_name in files:
+        full_fname = path.join(dir,file_name.strip())
+        if path.getsize(full_fname) == 0:
+            continue
+        with open(full_fname,'rt') as a:
+            layer_size, correct = parse_result_log(a)
+            res[layer_size].append( 1-float(correct[-1])/10000 )
+    
+    return res # {k: sorted(v) for k,v in res.items()}
+
+def plot_results_by_epoch(res):
     layer_sizes = sorted(res.keys())
     plt.figure()
     for ls in layer_sizes:
         plt.plot(res[ls],'.-')
     plt.legend(['ls'+str(l) for l in layer_sizes])
 
+def plot_results_by_layer_size(res):
+    layer_sizes = sorted(res.keys())
+    plt.figure()
+    for ls in layer_sizes:
+        plt.plot([ls]*len(res[ls]),res[ls],'+')
+    
 def main():
     args = arguments()
     if args.glob:
         file_list = list(glob.glob(path.join(args.dir, args.glob)))
         args.dir=''
     else:
-        file_list = args.files.split(",")
-    res = load_results(args.dir, file_list)
-    plot_results(res)
+        file_list = sorted(args.files.split(","))
+    if args.show_by == 'epoch':
+        res = load_results_by_epoch(args.dir, file_list)
+        plot_results_by_epoch(res)
+    else:
+        res = load_results_by_layer_size(args.dir, file_list)
+        plot_results_by_layer_size(res)
     plt.show()
     
 if __name__ == "__main__":
