f16a4373d99eaaf84d98ea091bc34bb55cc29fbe
kishkash555 <kishkash555@gmail.com>
2019-04-30 11:19:16 +0300

    hashnet - fix device compatibility 4

--- HashNet-pytorch/heavy_run2.sh
+++ HashNet-pytorch/heavy_run2.sh
@@ -1,10 +1,10 @@
 conda activate shahar
 git checkout parametric-pytorch-hashnet1
-python HashNet.py --device=cpu --log-interval=-1 --seed=4 --activation=relu --save-model &
-python HashNet.py --device=cpu --log-interval=-1 --seed=2 --activation=relu --save-model &
-python HashNet.py --device=cpu --log-interval=-1 --seed=1 --activation=relu --save-model &
-python HashNet.py --device=cpu --log-interval=-1 --seed=4 --activation=relu --save-model &
-python HashNet.py --device=cpu --log-interval=-1 --seed=2 --activation=relu --save-model &
-python HashNet.py --device=cpu --log-interval=-1 --seed=1 --activation=relu --save-model &
+python HashNet.py --device=cpu --log-interval=-1 --seed=4 --activation=relu --save-model --batch-size=32 --test-batch-size=32 &
+python HashNet.py --device=cpu --log-interval=-1 --seed=2 --activation=relu --save-model --batch-size=32 --test-batch-size=32 &
+python HashNet.py --device=cpu --log-interval=-1 --seed=1 --activation=relu --save-model --batch-size=32 --test-batch-size=32 &
+python HashNet.py --device=cpu --log-interval=-1 --seed=4 --activation=relu --save-model --batch-size=32 --test-batch-size=32 &
+python HashNet.py --device=cpu --log-interval=-1 --seed=2 --activation=relu --save-model --batch-size=32 --test-batch-size=32 &
+python HashNet.py --device=cpu --log-interval=-1 --seed=1 --activation=relu --save-model --batch-size=32 --test-batch-size=32 &
 
 
