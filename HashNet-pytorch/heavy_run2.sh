c9468d5af1af83e811c693f61b15ee7235b77da6
kishkash555 <kishkash555@gmail.com>
2019-05-23 17:20:39 +0300

    fix heavy_run2 script

--- HashNet-pytorch/heavy_run2.sh
+++ HashNet-pytorch/heavy_run2.sh
@@ -1,14 +1,14 @@
-python HashNet.py --device=cpu --k1=4900 --hidden=400 --k2=62  &
-python HashNet.py --device=cpu --k1=4900 --hidden=400 --k2=62  &
-python HashNet.py --device=cpu --k1=4900 --hidden=400 --k2=62  &
-python HashNet.py --device=cpu --k1=4900 --hidden=400 --k2=62  &
-python HashNet.py --device=cpu --k1=4900 --hidden=200 --k2=62  &
-python HashNet.py --device=cpu --k1=4900 --hidden=200 --k2=62  &
-python HashNet.py --device=cpu --k1=4900 --hidden=200 --k2=62  &
-python HashNet.py --device=cpu --k1=4900 --hidden=200 --k2=62  &
-python HashNet.py --device=cpu --k1=4900 --hidden=800 --k2=62  &
-python HashNet.py --device=cpu --k1=4900 --hidden=800 --k2=62  &
-python HashNet.py --device=cpu --k1=4900 --hidden=800 --k2=62  &
-python HashNet.py --device=cpu --k1=4900 --hidden=800 --k2=62  &
+python hashnet_main.py --device=cpu --k1=4900 --hidden=400 --k2=62  &
+python hashnet_main.py --device=cpu --k1=4900 --hidden=400 --k2=62  &
+python hashnet_main.py --device=cpu --k1=4900 --hidden=400 --k2=62  &
+python hashnet_main.py --device=cpu --k1=4900 --hidden=400 --k2=62  &
+python hashnet_main.py --device=cpu --k1=4900 --hidden=200 --k2=62  &
+python hashnet_main.py --device=cpu --k1=4900 --hidden=200 --k2=62  &
+python hashnet_main.py --device=cpu --k1=4900 --hidden=200 --k2=62  &
+python hashnet_main.py --device=cpu --k1=4900 --hidden=200 --k2=62  &
+python hashnet_main.py --device=cpu --k1=4900 --hidden=800 --k2=62  &
+python hashnet_main.py --device=cpu --k1=4900 --hidden=800 --k2=62  &
+python hashnet_main.py --device=cpu --k1=4900 --hidden=800 --k2=62  &
+python hashnet_main.py --device=cpu --k1=4900 --hidden=800 --k2=62  &
 
 
