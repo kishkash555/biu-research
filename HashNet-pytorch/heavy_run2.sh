1b44de351ff61df9fd5423e664f9bde8fa0d7248
Kishkash <kishkash555@gmail.com>
2019-05-23 18:45:19 +0300

    gradual launch to avoid log file conflicts

--- HashNet-pytorch/heavy_run2.sh
+++ HashNet-pytorch/heavy_run2.sh
@@ -1,14 +1,25 @@
 python hashnet_main.py --device=cpu --k1=4900 --hidden=400 --k2=62  --save-interval=500 &
+sleep 3
 python hashnet_main.py --device=cpu --k1=4900 --hidden=400 --k2=62  --save-interval=500 &
+sleep 3
 python hashnet_main.py --device=cpu --k1=4900 --hidden=400 --k2=62  --save-interval=500 &
+sleep 3
 python hashnet_main.py --device=cpu --k1=4900 --hidden=400 --k2=62  --save-interval=500 &
+sleep 3
 python hashnet_main.py --device=cpu --k1=4900 --hidden=200 --k2=62  --save-interval=500 &
+sleep 3
 python hashnet_main.py --device=cpu --k1=4900 --hidden=200 --k2=62  --save-interval=500 &
+sleep 3
 python hashnet_main.py --device=cpu --k1=4900 --hidden=200 --k2=62  --save-interval=500 &
+sleep 3
 python hashnet_main.py --device=cpu --k1=4900 --hidden=200 --k2=62  --save-interval=500 &
+sleep 3
 python hashnet_main.py --device=cpu --k1=4900 --hidden=800 --k2=62  --save-interval=500 &
+sleep 3
 python hashnet_main.py --device=cpu --k1=4900 --hidden=800 --k2=62  --save-interval=500 &
+sleep 3
 python hashnet_main.py --device=cpu --k1=4900 --hidden=800 --k2=62  --save-interval=500 &
+sleep 3
 python hashnet_main.py --device=cpu --k1=4900 --hidden=800 --k2=62  --save-interval=500 &
 wait
 
