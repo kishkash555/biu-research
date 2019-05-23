b0fdff33249781291fd721f55a5f20613c93fe85
kishkash555 <kishkash555@gmail.com>
2019-05-23 17:53:53 +0300

    fixes in heavy run script

--- HashNet-pytorch/heavy_run2.sh
+++ HashNet-pytorch/heavy_run2.sh
@@ -1,14 +1,14 @@
-python hashnet_main.py --device=cpu --k1=4900 --hidden=400 --k2=62  &
-python hashnet_main.py --device=cpu --k1=4900 --hidden=400 --k2=62  &
-python hashnet_main.py --device=cpu --k1=4900 --hidden=400 --k2=62  &
-python hashnet_main.py --device=cpu --k1=4900 --hidden=400 --k2=62  &
-python hashnet_main.py --device=cpu --k1=4900 --hidden=200 --k2=62  &
-python hashnet_main.py --device=cpu --k1=4900 --hidden=200 --k2=62  &
-python hashnet_main.py --device=cpu --k1=4900 --hidden=200 --k2=62  &
-python hashnet_main.py --device=cpu --k1=4900 --hidden=200 --k2=62  &
-python hashnet_main.py --device=cpu --k1=4900 --hidden=800 --k2=62  &
-python hashnet_main.py --device=cpu --k1=4900 --hidden=800 --k2=62  &
-python hashnet_main.py --device=cpu --k1=4900 --hidden=800 --k2=62  &
-python hashnet_main.py --device=cpu --k1=4900 --hidden=800 --k2=62  &
-
+python hashnet_main.py --device=cpu --k1=4900 --hidden=400 --k2=62  --save-interval=500 &
+python hashnet_main.py --device=cpu --k1=4900 --hidden=400 --k2=62  --save-interval=500 &
+python hashnet_main.py --device=cpu --k1=4900 --hidden=400 --k2=62  --save-interval=500 &
+python hashnet_main.py --device=cpu --k1=4900 --hidden=400 --k2=62  --save-interval=500 &
+python hashnet_main.py --device=cpu --k1=4900 --hidden=200 --k2=62  --save-interval=500 &
+python hashnet_main.py --device=cpu --k1=4900 --hidden=200 --k2=62  --save-interval=500 &
+python hashnet_main.py --device=cpu --k1=4900 --hidden=200 --k2=62  --save-interval=500 &
+python hashnet_main.py --device=cpu --k1=4900 --hidden=200 --k2=62  --save-interval=500 &
+python hashnet_main.py --device=cpu --k1=4900 --hidden=800 --k2=62  --save-interval=500 &
+python hashnet_main.py --device=cpu --k1=4900 --hidden=800 --k2=62  --save-interval=500 &
+python hashnet_main.py --device=cpu --k1=4900 --hidden=800 --k2=62  --save-interval=500 &
+python hashnet_main.py --device=cpu --k1=4900 --hidden=800 --k2=62  --save-interval=500 &
+wait
 
